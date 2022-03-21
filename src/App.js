27a696e7abd7392e20bff0aba7510c5e045b095f
Yash Goyal <yashgo0018@gmail.com>
2022-03-21 22:02:17 +0530

    updated app.jsx file to add hello world

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import {Web3ReactProvider} from '@web3-react/core';
-import {Web3Provider} from '@ethersproject/providers';
+import { Web3ReactProvider } from '@web3-react/core';
+import { Web3Provider } from '@ethersproject/providers';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 import Content from './components/Containers';
-import {Container, Image} from 'semantic-ui-react';
+import { Container, Image } from 'semantic-ui-react';
 import iconDevx from "./assets/icon-devx.svg"
 
 import './App.css';
@@ -18,19 +18,20 @@ const getLibrary = (provider) => {
 
 function App() {
   return (
-  <Web3ReactProvider getLibrary={getLibrary}>
-    <div className="App">
-      <Header/>
-      <Content>
-        <Container text textAlign='center'>
-          <Image centered size='tiny' src={iconDevx} style={{marginBottom:"2em"}}/>
-          <p>Create React Ethereum DApp on Polygon</p>
-          <p>Build your first app with Starter Kits</p>
-        </Container>
-      </Content>
-      <Footer/>
-    </div>
-  </Web3ReactProvider>
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <div className="App">
+        <Header />
+        <Content>
+          <Container text textAlign='center'>
+            <Image centered size='tiny' src={iconDevx} style={{ marginBottom: "2em" }} />
+            <p>Create React Ethereum DApp on Polygon</p>
+            <p>Build your first app with Starter Kits</p>
+            <p>hello world</p>
+          </Container>
+        </Content>
+        <Footer />
+      </div>
+    </Web3ReactProvider>
   );
 }
 
